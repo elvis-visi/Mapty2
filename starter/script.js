9def401506bb3c6811f1d9fa00b03548427bec36
elvis-visi <sivisevi1@gmail.com>
2023-10-11 13:11:54 +0200

    2.Displaying a Map Using Leaflet Library

--- starter/script.js
+++ starter/script.js
@@ -19,6 +19,22 @@ if (navigator) {
       const { latitude } = position.coords;
       const { longitude } = position.coords;
       console.log(`https://www.google.com/maps/@${latitude},${longitude},13z`);
+
+      //when we get the coords we want to display the map
+
+      const coords = [latitude, longitude];
+
+      const map = L.map('map').setView(coords, 13);
+
+      L.tileLayer('https://tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
+        attribution:
+          '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+      }).addTo(map);
+
+      L.marker(coords)
+        .addTo(map)
+        .bindPopup('A pretty CSS popup.<br> Easily customizable.')
+        .openPopup();
     },
     function () {
       //error cb
